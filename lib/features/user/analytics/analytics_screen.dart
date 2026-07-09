e41f301400eb1dcd399034cc098cee5ab30311b2
JONATHAN BRIANT WIJAYA <katabriant@gmail.com>
2026-07-09 15:54:31 +0700

    UPDATE: tab bar font style

--- lib/features/user/analytics/analytics_screen.dart
+++ lib/features/user/analytics/analytics_screen.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import '5r findings/analytics_5r_tab.dart';
 import 'kts production/analytics_kts_tab.dart';
 import 'accident report/analytics_accident_tab.dart';
 import 'preventif_maintenance/analytics_preventif_tab.dart';
 
-// MAIN SCREEN
 class AnalyticsScreen extends StatefulWidget {
   final String lang;
   const AnalyticsScreen({super.key, required this.lang});
@@ -20,7 +20,6 @@ class _AnalyticsScreenState extends State<AnalyticsScreen> {
   // FINDING TYPE SELECTOR
   String _selectedFindingType = '5R';
 
-  // BUILD METHOD
   @override
   Widget build(BuildContext context) {
     // KTS PRODUCTION
@@ -143,7 +142,7 @@ class _AnalyticsScreenState extends State<AnalyticsScreen> {
                         const SizedBox(width: 4),
                         Flexible(child: Text(
                           t['label'] as String,
-                          style: TextStyle(
+                          style: GoogleFonts.poppins(
                             fontSize: 10, fontWeight: FontWeight.w700,
                             color: isSelected ? Colors.white : activeColor,
                           ),
